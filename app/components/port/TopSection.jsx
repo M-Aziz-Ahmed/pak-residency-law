3881bb726783315374adc2d6e79c514d2d48895b
Hammad Saleem <hammadsaleem2003@gmail.com>
2026-01-12 23:53:36 +0500

    top section text addition

--- app/components/port/TopSection.jsx
+++ app/components/port/TopSection.jsx
@@ -1,7 +1,16 @@
 export default () => {
     return (
         <section>
-            <div className="flex">
+            <div className="flex bg-light">
+                <div className="textsection py-10 px-15 font-bold">
+                    <h1 className="text-5xl">Smart Legal help</h1>
+                    <h1 className="text-5xl">for</h1>
+                    <h1 className="text-5xl text-green-400">property disputes</h1>
+                    <p className="text-gray-600 ">Navigate complex residential laws in Urdu,Roman</p>
+                 <p className="text-gray-600">Urdu or English.Analyze deeds,identify risks and </p>
+                 <p className="text-gray-600">Urdu or English.Analyze deeds,identify risks and </p>
+
+                </div>
                 
             </div>
         </section>
--- app/portfollio/page.js
+++ app/portfollio/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import TopSection from "../components/port/TopSection";
 
 export default function Home() {
   return (
  <>
- 
+ <TopSection>
+  
+ </TopSection>
  </>
   );
 }
