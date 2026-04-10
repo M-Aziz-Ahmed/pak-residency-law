6c7fdbf5613c0cff0ee09498d25178b35dd7c7d9
Hammad Saleem <hammadsaleem2003@gmail.com>
2026-04-10 23:58:34 +0500

    nextconfigchanges

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  serverExternalPackages: ['mongoose', 'bcrypt'],
+ experimental: {
+  serverComponentsExternalPackages: [...]
+},
 };
 
 export default nextConfig;
